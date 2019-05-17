3c5111c2d78b020534ddb09082310313d4d6c05a
benjopw <benyaminpurbowasito@gmail.com>
2019-05-17 14:43:28 +0700

    bisa gerak

--- animationView/ViewController.swift
+++ animationView/ViewController.swift
@@ -10,8 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+   
+    @IBOutlet weak var mainView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(mainView.frame)
+        print("Nilai X = \(mainView.frame.origin.x)")
+        print("Nilai Y = \(mainView.frame.origin.y)")
+        print("Nilai Tinggi = \(mainView.frame.height)")
+        print("Nilai lebar = \(mainView.frame.width)")
+        
+        UIView.animate(withDuration: 5) {
+            self.mainView.frame.origin.x = 200
+            self.mainView.frame.origin.y = 200
+            print(self.mainView.frame)
+            print("Nilai X = \(self.mainView.frame.origin.x)")
+            print("Nilai Y = \(self.mainView.frame.origin.y)")
+        }
+        
         // Do any additional setup after loading the view.
     }
 
