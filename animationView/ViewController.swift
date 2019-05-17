dee5f8863f9c1a0acd94f3a7adc072fa57d52262
benjopw <benyaminpurbowasito@gmail.com>
2019-05-17 14:25:04 +0700

    Initial Commit

---
+++ animationView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  animationView
+//
+//  Created by Benjamin Purbowasito on 17/05/19.
+//  Copyright © 2019 iosda Ben. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
